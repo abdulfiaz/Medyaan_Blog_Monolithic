afb1e1653ecc3fde383e98d82318cefbb7c3a832
Praveenh682 <138794818+Praveenh682@users.noreply.github.com>
2024-11-05 13:30:48 +0530

    [ADD]postcategory

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from adminapp.models import BaseModel,IUMaster
 
-# Create your models here.
+
+class PostCategory(BaseModel):
+    category_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100,null=True,blank=True)
+    is_archived = models.BooleanField(default=False)
+    iu_id = models.ForeignKey(IUMaster,related_name='postcategory_iu',on_delete = models.CASCADE)
+
+    class Meta:
+        db_table = 'PostCategory'
+        ordering = ['created_at']
